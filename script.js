aa53b1255d31b999eabf8e4657c2d7538106078d
a5med <ayassine.auce@gmail.com>
2024-11-01 10:00:01 +0200

    display the result

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 fetch("http://www.omdbapi.com/?t=Game of Thrones&Season=1&Episode=1&apikey=b95d0e24")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        document.body.innerHTML = `<h1> movie title ${data.Title}</h1>`
+    })
 
-document.body.innerHTML = `<h1> movie title ${data.title}</h1>`
+
 
 
 
