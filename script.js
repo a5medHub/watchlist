26d46008f5f0e7f5a2640e54c0ade71c2f5c5997
a5med <ayassine.auce@gmail.com>
2024-11-14 17:50:15 +0200

    new integration

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputMovie = document.getElementById("input-movie")
 const submitSearch = document.getElementById("submit-search")
+const listedMovie = document.getElementById("listed-movie")
+let movieData = []
 
 submitSearch.addEventListener("click", ()=>{
     console.table("search button clicked", inputMovie.value)
@@ -17,12 +19,41 @@ function callTheMovieForSearchMovie(movieNameParam) {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
+        movieData.push(data)
         // document.body.innerHTML = `<h1> movie title ${data.Title}</h1>`
     })
+    listingMovies()
 }
 callTheMovieForSearchMovie("blade runner") 
 
 // http://www.omdbapi.com/?i=tt3896198&apikey=b95d0e24
+function listingMovies() {
+    const movie = movieData.forEach(e => {
+        return e
+    },0)
+    console.log("movie", movie)
+    // listedMovie.innerHTML =`
+    //     <div>
+    //         <img style="height: 200px;" src="https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg" alt="image for the movie">
+    //     </div>
+    //     <div>
+    //         <div id="name-section">
+    //             <h5>${movieData.Title}</h5>
+    //             <p>MOVIE RATING</p>
+    //         </div>
+    //         <div id="theme-section">
+    //             <p>MOVIE TIME</p>
+    //             <p>MOVIE THEME</p>
+    //             <p>ASS TO WATCHLIST</p>
+    //         </div>
+    //         <div id="details-section">
+    //             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    //                 Voluptas mollitia tempora necessitatibus suscipit ullam qui aperiam at praesentium repellendus cum! Culpa quod, 
+    //                 neque adipisci cumque ex cum quisquam ea nesciunt.
+    //             </p>
+    //         </div>
+    //     </div>`
 
-
+}
+    console.log(movieData)
