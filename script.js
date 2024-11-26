7c94d74ce9970e8e50cafab0a2544f8a0e3dbc89
a5med <ayassine.auce@gmail.com>
2024-11-26 19:44:19 +0200

    debuging the api responce

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
+import {v4 as uuidv4} from 'https://jspm.dev/uuid';
 const inputMovie = document.getElementById("input-movie")
 const submitSearch = document.getElementById("submit-search")
 const listedMovie = document.getElementById("listed-movie")
 let movieData = []
 
+
 submitSearch.addEventListener("click", ()=>{
-    console.table("search button clicked", inputMovie.value)
     let theMovie = inputMovie.value
     callTheMovieForSearchMovie(theMovie)
 })
 function renderMovieTemplateFromApiCall() {
 
 }
-
+/* this function is for the submit button to take the parameter 
+   from the input field and pass the data to the url of fetch */
 function callTheMovieForSearchMovie(movieNameParam) {
     console.log("movieNameParam: "+ movieNameParam)
     const privateKey = "apikey=b95d0e24"
-    // let movieName = "t=Game of Thrones&Season=1&Episode=1"
-    let movieName = "t="+movieNameParam
+    let movieName = "t="+movieNameParam // the T stand for the movie title
     let url = "http://www.omdbapi.com/?"+privateKey+"&"+movieName
 
-    fetch(url)
-    .then(res => res.json())
+    fetch(url,{mode: 'no-cors'})
+    .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json(); // Convert the response to JSON
+      })
     .then(data => {
-        // console.log(data)
         movieData.push(data)
+        // console.log("the fetch data is: ",data)
         // document.body.innerHTML = `<h1> movie title ${data.Title}</h1>`
     })
-    listingMovies()
+    // .catch(error => console.error('Error:', error));
+    // listingMovies()
+    // console.log(movieData)
+    setTimeout(listingMovies, 100)
 }
-callTheMovieForSearchMovie("blade runner") 
 
 // http://www.omdbapi.com/?i=tt3896198&apikey=b95d0e24
 function listingMovies() {
-    const movie = movieData.forEach(e => {
-        return e
-    },0)
-    console.log("movie", movie)
-    // listedMovie.innerHTML =`
-    //     <div>
-    //         <img style="height: 200px;" src="https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg" alt="image for the movie">
-    //     </div>
-    //     <div>
-    //         <div id="name-section">
-    //             <h5>${movieData.Title}</h5>
-    //             <p>MOVIE RATING</p>
-    //         </div>
-    //         <div id="theme-section">
-    //             <p>MOVIE TIME</p>
-    //             <p>MOVIE THEME</p>
-    //             <p>ASS TO WATCHLIST</p>
-    //         </div>
-    //         <div id="details-section">
-    //             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    //                 Voluptas mollitia tempora necessitatibus suscipit ullam qui aperiam at praesentium repellendus cum! Culpa quod, 
-    //                 neque adipisci cumque ex cum quisquam ea nesciunt.
-    //             </p>
-    //         </div>
-    //     </div>`
+    movieData.forEach(e => {
+        console.log("movie ", e)
+        console.log(`
+            movie title  ${e.Title} 
+            rating: ${e.imdbRating}
+            duration: ${e.Runtime}
+            theme: ${e.Genre}
+            breaf: ${e.Plot}
+            image: ${e.Poster}`)
+       
+            
+            // listedMovie.innerHTML +=`
+            // <div>
+            //     <div>
+            //         <img style="height: 200px;" src="${e.Poster}" alt="image for the movie">
+            //     </div>
+            //     <div>
+            //         <div id="name-section">
+            //             <h5>${e.Title}</h5>
+            //             <p>${e.imdbRating}</p>
+            //         </div>
+            //         <div id="theme-section">
+            //             <p>${e.Runtime}</p>
+            //             <p>${e.Genre}</p>
+            //             <p>Add TO WATCHLIST</p>
+            //         </div>
+            //         <div id="details-section">
+            //             <p>${e.Plot}</p>
+            //         </div>
+            //     </div>
+            // </div>`
+        },0)
 
 }
     // console.log(movieData)
